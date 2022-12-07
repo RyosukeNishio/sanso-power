369f6cb61919c738acf650272cbc550dffa9c958
RyosukeNishio <info@dogcat-clear.com>
2022-12-08 00:40:16 +0900

    add:　初回アップ

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+  <main>ddd
+    <nav class="nav">
+      <ul>
+        <li><a href="">aaa</a></li>
+        <li><a href="">bbb</a></li>
+        <li><a href="">ccc</a></li>
+        <li><a href="">ddd</a></li>
+        <li><a href="">eee</a></li>
+      </ul>
+    </nav>
+  </main>
+
+<?php get_footer(); ?>
