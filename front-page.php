0a6f39e38c4ffecfb55ffc20d083fb6fe2af35ec
RyosukeNishio <info@dogcat-clear.com>
2022-12-09 10:06:16 +0900

    feat: 途中経過をアップ

--- front-page.php
+++ front-page.php
@@ -1,18 +1,75 @@
 <?php get_header(); ?>
 
   <main>
-    <nav class="nav">
-    <?php echo get_field('test_'); ?>
 
+  <h1 class="top_ttl">酸素系エコ洗剤（多目的で無公害なエコ洗剤）を作り続けて３０年のタイシン九州です。</h1>
+    
+  
+  <section class="top_fv max_1100">
+    <div class="nav">
+      <h2>お掃除・お洗濯方法で探す</h2>
       <ul class="usage_nav">
         <?php if( have_rows('loop_usage') ): ?>
-        <?php while ( have_rows('loop_usage') ) : the_row(); ?>
+          <?php while ( have_rows('loop_usage') ) : the_row(); ?>
           <li><a href="<?php echo home_url() ?><?php the_sub_field('loop_usage_link'); ?>"><?php the_sub_field('loop_usage_text'); ?></li></a>
-        <?php endwhile; ?>
-        <?php endif; ?>
+          <?php endwhile; ?>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </section>
+    
+  <section class="recommend">
+    <ul class="slider">
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog01.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog02.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog03.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog01.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog02.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog03.jpg"><span>記事タイトル</span></a></li>
+      <li><a href=""><img src="https://junpei-sugiyama.com/wp-content/uploads/2021/01/dog01.jpg"><span>記事タイトル</span></a></li>
+    </ul>
+  </section>
+
+  <section class="campaign max_1100">
+    <ul class="campaign_bnr">
+      <li><a href="">キャンペーンのバナーが入る</a></li>
+      <li><a href="">キャンペーンのバナーが入る</a></li>
+    </ul>
+  </section>
+
+  <section class="max_1100 approach">
+    <div class="approach_news">
+      <h2>新着情報</h2>
+      <ul>
+        <li><a href="">2222年2月22日 新着情報、お知らせの記事タイトル</a></li>
+        <li><a href="">2222年2月22日 新着情報、お知らせの記事タイトル</a></li>
+        <li><a href="">2222年2月22日 新着情報、お知らせの記事タイトル</a></li>
+        <li><a href="">2222年2月22日 新着情報、お知らせの記事タイトル</a></li>
       </ul>
-    </nav>
+    </div>
+
+    <div class="approach_voice">
+      <h2>お客様の声</h2>
+        <ul>
+          <li><a href="">2222年2月22日 お客様の声の記事タイトル</a></li>
+          <li><a href="">2222年2月22日 お客様の声の記事タイトル</a></li>
+          <li><a href="">2222年2月22日 お客様の声の記事タイトル</a></li>
+          <li><a href="">2222年2月22日 お客様の声の記事タイトル</a></li>
+      </ul>
+    </div>
+  </section>
+  
 
   </main>
+  
+  <script>
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        slidesToShow: 6,
+        infinite: true,
+    })
+</script>
+
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -4,11 +4,34 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png" sizes="180x180">
   <title>Document</title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/slick.css">
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/slick.js"></script>
+
+
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 </head>
 <body>
 
   <header class="top_header">
+    <nav>
+      <h1><a href="">LOGO</a></h1>
+      <ul>
+        <li><a href="">かわゆい花子とは</a></li>
+        <li><a href="">お客様の声</a></li>
+        <li><a href="">商品一覧</a></li>
+        <li><a href="">よくあるFAQ</a></li>
+        <li><a href="">ご注文〜お届け</a></li>
+        <li><a href="">お問い合わせ</a></li>
+        <li><a href="">カートへ</a></li>
+      </ul>
+    </nav>
 
   </header>
