f8af5aef779c2c1e72ccef8f39eddf3e68a55031
RyosukeNishio <info@dogcat-clear.com>
2022-12-13 13:35:08 +0900

    fix: 途中経過をアップ

--- footer.php
+++ footer.php
@@ -1,5 +1,112 @@
-<footer>
-  footer
+<footer class="footer">
+  <article class="footer_link">
+    <div>
+      <h2>酸素系エコ洗剤について</h2>
+      <ul class="footer_list">
+        <li><a href="../hanako_jiten/index.html">酸素系エコ洗剤「かわゆい花子」って何？</a></li>
+        <li><a href="../hanako_jiten/hanako_hiwa.html">「かわゆい花子」誕生秘話</a></li>
+        <li><a href="../master/index.html">花子マスターにインタビュー</a></li>
+        <li><a href="../joho/kawayuihanako.html#sansokei">酸素系エコ洗剤の主原料</a></li>
+        <li><a href="../joho/knowhow.html">酸素系エコ洗剤の使い方</a></li>
+        <li><a href="../okyakusama_koe/index.html">酸素系エコ洗剤お客様の声</a></li>
+        <li><a href="../koe/index.html">各業種でお役立ち</a></li>
+        <li><a href="../joho/index.html">酸素系エコ洗剤商品一覧</a></li>
+        <li><a href="http://sansoriki.jp/hanako/hanako/index.html" target="_blank">酸素系エコ洗剤ご購入</a></li>
+        <li><a href="../syohin/ecopre.html">酸素系エコ洗剤<br>;エコプレミアム選定！</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>酸素系エコ洗剤使用例（1）</h2>
+      <ul class="footer_list">
+        <li><a href="../abura/index.html">油汚れ</a>
+          <ul class="footer_list_inner">
+            <li><a href="../abura/kankisen.html">換気扇</a></li>
+            <li><a href="../abura/gas.html">ガスレンジ</a></li>
+            <li><a href="../abura/yakan.html">やかん</a></li>
+            <li><a href="../abura/chuubou.html">給食室・厨房</a></li>
+            <li><a href="../abura/fryer.html">フライヤー</a></li>
+            <li><a href="../abura/filter.html">グリスフィルター</a></li>
+          </ul>
+        </li>
+        <li><a href="../chashibu/index.html">茶渋</a>
+          <ul class="footer_list_inner">
+            <li><a href="../chashibu/kyuusu.html">急須</a></li>
+            <li><a href="../chashibu/suitou.html">水筒</a></li>
+            <li><a href="../chashibu/coffee.html">コーヒーカップ</a></li>
+            <li><a href="../chashibu/glass.html">グラス</a></li>   
+          </ul>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>酸素系エコ洗剤使用例（2）</h2>
+      <ul class="footer_list">
+        <li><a href="../souji/index.html">普段のお掃除</a>
+          <ul class="footer_list_inner">
+            <li><a href="../souji/kabe.html">壁紙（ヤニ）</a></li>
+            <li><a href="../souji/mado.html">窓</a></li>
+            <li><a href="../souji/nagashi.html">流し台</a></li>
+            <li><a href="../example/index.html">大掃除</a></li>
+            <li><a href="../souji/hikkoshi.html">お引越し</a></li>
+          </ul>
+        </li>
+        <li><a href="../souji/index.html">家電・設備のお掃除</a>
+          <ul class="footer_list_inner">
+            <li><a href="../souji/reizouko.html">冷蔵庫</a></li>
+            <li><a href="../souji/aircon.html">エアコン</a></li>
+            <li><a href="../souji/sentakuso.html">洗濯槽</a></li>
+            <li><a href="../souji/furogama.html">風呂釜</a></li>
+            <li><a href="../souji/haisui.html">排水口</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>酸素系エコ洗剤使用例（3）</h2>
+      <ul class="footer_list">
+        <li><a href="../sentaku/index.html">お洗濯</a>
+          <ul class="footer_list_inner">
+            <li><a href="../sentaku/waishatsu.html">ワイシャツのシミ</a></li>
+            <li><a href="../sentaku/ripagaze.html">リパガーゼのシミ</a></li>
+            <li><a href="../sentaku/kibami.html">衣替えのシャツの黄ばみ</a></li>
+            <li><a href="../sentaku/kattashatsu.html">カッターシャツの襟汚れ</a></li>
+            <li><a href="../sentaku/judo.html">柔道着・空手着漂白</a></li>
+            <li><a href="../sentaku/kutsusita.html">靴下・足袋</a></li>
+            <li><a href="../sentaku/wear.html">スポーツウェアの袖口</a></li>
+            <li><a href="../sentaku/fukin.html">ふきん漂白</a></li>
+            <li><a href="../sentaku/sentakumono_namagawaki.html">洗濯物の臭い対策</a></li>
+            <li><a href="../sentaku/kaigo.html">要介護者の洗濯物に</a></li>
+          </ul>
+      
+        </li>
+        <li><a href="../big/index.html">大物お洗濯</a></li>
+        <li><a href="../big/sheets.html">シーツ・ソファーカバー</a></li>
+        <li><a href="../big/curtain.html">カーテン・レースカーテン</a></li>
+        <li><a href="../big/boushi.html">帽子</a></li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>ご案内</h2>
+      <ul class="footer_list">
+        <li><a href="../">このサイトのＨＯＭＥへ</a></li>
+        <li><a href="../joho/gotyuumon.html">ご注文に関しまして</a></li>
+        <li><a href="../joho/gotyuumon.html#gotyuumon">ご注文方法</a></li>
+        <li><a href="../joho/gotyuumon.html#osiharai">お支払い方法</a></li>
+        <li><a href="../joho/gotyuumon.html#haisou">配送・送料について</a></li>
+        <li><a href="../joho/media.html">お掃除メディア情報</a></li>
+        <li><a href="../joho/kakeizu.html">タイシン秘密の家系図</a></li>
+        <li><a href="../joho/link.html">リンク集</a></li>
+        <li><a href="../joho/qanda.html">・よくあるＱ ＆ Ａ</a></li>
+        <li><a href="../form/otoiawase.html">お問い合わせフォーム</a></li>
+      </ul>
+    </div>
+  </article>
+
+  <?php echo do_shortcode('[sbc title="no"]'); ?>
 </footer>
 
 </body>
